d40246bcc198f671abc1ed44b563b0889f6b4f47
Radhika Patel <radhika.patel2206@gmail.com>
2020-05-14 18:57:43 +0530

    pip freeze missing requests

--- radzlist/models.py
+++ radzlist/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Search(models.Model):
+
+    __tablename__ = 'searches'
+    
     search = models.CharField(max_length=500)
     created = models.DateTimeField(auto_now=True)
     
